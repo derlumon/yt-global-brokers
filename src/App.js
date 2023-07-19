153fb94cba7556f0eff8b4e4d2802a21bd9ebb8b
Endersis <alexisgt7168@gmail.com>
2023-07-19 10:56:44 -0600

    fiked component servicio and button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from "@emotion/react";
 import Navbar from "./Body/navbar";
 import Start from "./Body/start";
 import Box from "@mui/material/Box"
-
+import { PiPenThin, PiScalesThin, PiChartLineUpThin, PiBooksThin, PiWarehouseThin, PiStorefrontThin } from "react-icons/pi";
 import ButtonDial from "./Body/ButtonDial";
 import Servicios from "./Body/Servicios";
 import Ubicanos from "./Body/Ubicanos";
@@ -16,6 +16,72 @@ import { LanguageProvider } from "./Context/LanguageContext";
 import ButtonBaseDemo from "./Body/Consulta";
 
 function App() {
+  const buttons = [
+    {
+      icon: <PiPenThin />,
+      title: "Contract Drafting & Review",
+      lists: [
+        "Sales & Purchase Agreement (SPA)",
+        "Non-Disclosure Agreement (NDA)",
+        "Manufacturing Agreement (NNN)",
+        "Supplier OEM Agreement (OEM)",
+      ],
+    },
+    {
+      icon: <PiScalesThin />,
+      title: "Contract Drafting & Review",
+      lists: [
+        "Sales & Purchase Agreement (SPA)",
+        "Non-Disclosure Agreement (NDA)",
+        "Manufacturing Agreement (NNN)",
+        "Supplier OEM Agreement (OEM)",
+      ],
+    },
+    {
+      icon: <PiChartLineUpThin />,
+      title: "Contract Drafting & Review",
+      lists: [
+        "Sales & Purchase Agreement (SPA)",
+        "Non-Disclosure Agreement (NDA)",
+        "Manufacturing Agreement (NNN)",
+        "Supplier OEM Agreement (OEM)",
+      ],
+    },
+    {
+      icon: <PiBooksThin />,
+      title: "Contract Drafting & Review",
+      lists: [
+        "Sales & Purchase Agreement (SPA)",
+        "Non-Disclosure Agreement (NDA)",
+        "Manufacturing Agreement (NNN)",
+        "Supplier OEM Agreement (OEM)",
+      ],
+    },
+    {
+      icon: <PiWarehouseThin />,
+      title: "Contract Drafting & Review",
+      lists: [
+        "Sales & Purchase Agreement (SPA)",
+        "Non-Disclosure Agreement (NDA)",
+        "Manufacturing Agreement (NNN)",
+        "Supplier OEM Agreement (OEM)",
+      ],
+    },
+    {
+      icon: <PiStorefrontThin />,
+      title: "Contract Drafting & Review",
+      lists: [
+        "Sales & Purchase Agreement (SPA)",
+        "Non-Disclosure Agreement (NDA)",
+        "Manufacturing Agreement (NNN)",
+        "Supplier OEM Agreement (OEM)",
+      ],
+    },
+    
+    
+    
+    
+  ];
   return (
     <LanguageProvider>
       <ThemeProvider theme={theme}>
@@ -24,7 +90,7 @@ function App() {
         <Box width={"100%"}>
           <Introduccion/>
           <ButtonBaseDemo/>
-          <Servicios/>
+          <Servicios buttons={buttons}/>
           
           <Contactanos/>
           <Ubicanos/>
--- src/Body/Button.js
+++ src/Body/Button.js
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
-import {
-  Card,
-  CardContent,
-  Typography,
-} from "@mui/material";
-
+import { Card, CardContent, Typography } from "@mui/material";
 import "./button.css";
 
 export default function Button(props) {
@@ -17,60 +12,59 @@ export default function Button(props) {
   const handleMouseOut = () => {
     setShowMore(false);
   };
+
   const handleClick = () => {
-    
-   
+    // Handle button click event
   };
 
   return (
     <div>
-     
-        <Card
-          sx={{   m: { xs: "6px",sm:"6px" },
-          width: { md: "350px", xs: "330px" } }}
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-          className={`highlighted-card ${showMore ? "show-more" : ""}`}
-          elevation={showMore ? 3 : 1}
-          onClick={handleClick}
-        >
-          <CardContent>
-            {props.icon &&
-              React.cloneElement(props.icon, {
-                className: `icon ${showMore ? "show-more" : ""} ${
-                  showMore ? "fade-out" : ""
-                }`,
-                style: { fontSize: "3rem", marginLeft: "-210px" },
-              })}
+      <Card
+        sx={{
+          m: { xs: "6px", sm: "6px" },
+          width: { md: "350px", xs: "330px" },
+        }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        className={`highlighted-card ${showMore ? "show-more" : ""}`}
+        elevation={showMore ? 3 : 1}
+        onClick={handleClick}
+      >
+        <CardContent>
+          {props.icon &&
+            React.cloneElement(props.icon, {
+              className: `icon ${showMore ? "show-more" : ""} ${
+                showMore ? "fade-out" : ""
+              }`,
+              style: { fontSize: "3rem", marginLeft: "-210px" },
+            })}
 
-            <div className={`title ${showMore ? "show-more" : ""}`}>
-              <Typography variant="h5" component="div">
-                <b> {props.title}</b>
-              </Typography>
-            </div>
-
-            <ul className={`feature-list  ${showMore ? "show-more" : ""}`}>
-              <li>{props.lista1}</li>
-              <li>{props.lista2}</li>
-              <li>{props.lista2}</li>
-              <li>{props.lista3}</li>
-              <li>{props.lista4}</li>
-            </ul>
-
-            <Typography
-              sx={{
-                fontSize: "1rem",
-                marginLeft: "-150px",
-                marginBottom: "-10px",
-                marginTop: "-30px",
-              }}
-              variant="body2"
-              className={`show-more-text ${showMore ? "show-more" : ""}`}
-            >
-              <b>Learn more -{">"}</b>
+          <div className={`title ${showMore ? "show-more" : ""}`}>
+            <Typography variant="h5" component="div">
+              <b>{props.title}</b>
             </Typography>
-          </CardContent>
-        </Card>
+          </div>
+
+          <ul className={`feature-list ${showMore ? "show-more" : ""}`}>
+            {props.lists.map((listItem, index) => (
+              <li key={index}>{listItem}</li>
+            ))}
+          </ul>
+
+          <Typography
+            sx={{
+              fontSize: "1rem",
+              marginLeft: "-150px",
+              marginBottom: "-10px",
+              marginTop: "-30px",
+            }}
+            variant="body2"
+            className={`show-more-text ${showMore ? "show-more" : ""}`}
+          >
+            <b>Learn more -{">"}</b>
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
