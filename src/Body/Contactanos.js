947ffcff9e69bc1c0b1f07bfee36ab8dc47b1bb3
Endersis <alexisgt7168@gmail.com>
2023-07-20 19:16:46 -0600

    features:new animation

--- src/Body/Contactanos.js
+++ src/Body/Contactanos.js
@@ -1,8 +1,9 @@
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { languages } from "../Languages/languages";
 import { useContext } from "react";
 import { LanguageContext } from "../Context/LanguageContext";
+
 const ContactForm = () => {
   const { currentLanguage } = useContext(LanguageContext);
   const [formData, setFormData] = useState({
@@ -39,27 +40,64 @@ const ContactForm = () => {
     )}`;
   };
 
+  const contactFormRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0, // The ratio of the element's visibility before triggering the animation
+    };
+
+    const handleIntersect = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in");
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+    const elements = contactFormRef.current.querySelectorAll(".animate-fade-in");
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <Box sx={{ width: "100%", textAlign: "center" }}>
+    <Box sx={{ width: "100%", textAlign: "center" }} ref={contactFormRef}>
       <Box
         sx={{
           backgroundColor: "#848688",
           justifyContent: "center",
-          alignContent:"center"
+          alignContent: "center",
         }}
+        className="animate-fade-in"
       >
-        <Typography variant="h4" gutterBottom color="white" fontFamily="Rubik "alignContent="center" sx={{padding:{md:"3rem",xs:"2rem"}}}>
-        {languages[currentLanguage].contactanos.title}
+        <Typography
+          variant="h4"
+          gutterBottom
+          color="white"
+          fontFamily="Rubik "
+          alignContent="center"
+          sx={{ padding: { md: "3rem", xs: "2rem" } }}
+        >
+          {languages[currentLanguage].contactanos.title}
         </Typography>
       </Box>
-      <Box sx={{paddingX:{xs:"5%",md:"10%"},paddingY:"5%"}}>
-        <Typography variant="h4" align="center" gutterBottom>
-        {languages[currentLanguage].contactanos.subtitle}
+      <Box sx={{ paddingX: { xs: "5%", md: "10%" }, paddingY: "5%" }}>
+        <Typography variant="h4" align="center" gutterBottom className="animate-fade-in">
+          {languages[currentLanguage].contactanos.subtitle}
         </Typography>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="animate-fade-in">
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6} className="animate-fade-in">
               <TextField
                 fullWidth
                 label={languages[currentLanguage].contactanos.name}
@@ -69,7 +107,7 @@ const ContactForm = () => {
                 required
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6} className="animate-fade-in">
               <TextField
                 fullWidth
                 label={languages[currentLanguage].contactanos.direction}
@@ -79,7 +117,7 @@ const ContactForm = () => {
                 required
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6} className="animate-fade-in">
               <TextField
                 fullWidth
                 label={languages[currentLanguage].contactanos.email}
@@ -90,7 +128,7 @@ const ContactForm = () => {
                 required
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6} className="animate-fade-in">
               <TextField
                 fullWidth
                 label={languages[currentLanguage].contactanos.numberphone}
@@ -100,7 +138,7 @@ const ContactForm = () => {
                 required
               />
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12} className="animate-fade-in">
               <TextField
                 fullWidth
                 label={languages[currentLanguage].contactanos.description}
@@ -118,16 +156,12 @@ const ContactForm = () => {
               sm={6}
               md={6}
               sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+              className="animate-fade-in"
             >
               {/* Placeholder Grid item to occupy the space on larger screens */}
             </Grid>
-            <Grid item xs={12} >
-              <Button
-                type="submit"
-                variant="contained"
-                color="inherit"
-                fullWidth
-              >
+            <Grid item xs={12} className="animate-fade-in">
+              <Button type="submit" variant="contained" color="inherit" fullWidth>
                 {languages[currentLanguage].contactanos.send}
               </Button>
             </Grid>
--- src/Body/PracticeArea.js
+++ src/Body/PracticeArea.js
@@ -1,15 +1,40 @@
 import { Grid, Typography, Box } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./servicios.css";
 
 export default function PracticeArea(props) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const topPosition = practiceAreaRef.current.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (topPosition < windowHeight * 0.8) {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const practiceAreaRef = React.useRef(null);
+
   return (
     <Box
+      ref={practiceAreaRef}
       sx={{
         backgroundColor: "#DFDFDF",
         textAlign: "center",
         width: "fullWidth",
         paddingX: { xs: "5%", md: "10%" },
         paddingY: "5%",
+        opacity: isVisible ? 1 : 0,
+        transform: `translateY(${isVisible ? "0" : "20px"})`,
+        transition: "opacity 1s, transform 1s",
       }}
     >
       <Typography
